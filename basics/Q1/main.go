e04c483317105ed76259cf511b46a713b5e468b1
elyar <elyarnejati@gmail.com>
2024-07-10 19:26:05 +0330

    add Q1

--- basics/Q1/main.go
+++ basics/Q1/main.go
@@ -1,3 +1,17 @@
 package main
 
+import "fmt"
 
+func main() {
+	var base float64
+	var height float64
+
+	fmt.Print("Enter base: ")
+	fmt.Scanln(&base)
+	fmt.Print("Enter height: ")
+	fmt.Scanln(&height)
+	
+	area := base * height
+
+	fmt.Println("Area is:", area)
+}
