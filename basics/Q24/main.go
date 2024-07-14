26954e66327d524ebfd46e0a68f9e60ce063ef52
elyar <elyarnejati@gmail.com>
2024-07-14 11:53:17 +0330

    add Q24

---
+++ basics/Q24/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		k float64
+		x float64
+		n float64
+	)
+	fmt.Print("Enter k: ")
+	fmt.Scan(&k)
+	fmt.Print("Enter x: ")
+	fmt.Scan(&x)
+	fmt.Print("Enter n: ")
+	fmt.Scan(&n)
+	acceleration := (k - x) * 60 / n
+	fmt.Println("acceleration is", acceleration)
+
+}
