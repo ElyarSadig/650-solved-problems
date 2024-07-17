a8d48627139a1dece23a58417d4be9a202d01a35
elyar <elyarnejati@gmail.com>
2024-07-17 12:38:25 +0330

    add Q36

---
+++ basics/Q36/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		a float64
+		b float64
+		h float64
+	)
+	fmt.Print("Enter a: ")
+	fmt.Scan(&a)
+	fmt.Print("Enter b: ")
+	fmt.Scan(&b)
+	fmt.Print("Enter h: ")
+	fmt.Scan(&h)
+	s := h * (a + b) / 2
+	fmt.Println("S is", s)
+}
