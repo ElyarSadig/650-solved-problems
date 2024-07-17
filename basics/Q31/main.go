77f6d3c7654dbd3ec2dd79f3ba5da08b68b2e2f8
elyar <elyarnejati@gmail.com>
2024-07-17 12:17:59 +0330

    add Q31

---
+++ basics/Q31/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Number\tSquare\tCube")
+	fmt.Printf("%d\t%d\t%d\n", 0, 0*0, 0*0*0)
+	fmt.Printf("%d\t%d\t%d\n", 1, 1*1, 1*1*1)
+	fmt.Printf("%d\t%d\t%d\n", 2, 2*2, 2*2*2)
+	fmt.Printf("%d\t%d\t%d\n", 3, 3*3, 3*3*3)
+	fmt.Printf("%d\t%d\t%d\n", 4, 4*4, 4*4*4)
+	fmt.Printf("%d\t%d\t%d\n", 5, 5*5, 5*5*5)
+	fmt.Printf("%d\t%d\t%d\n", 6, 6*6, 6*6*6)
+	fmt.Printf("%d\t%d\t%d\n", 7, 7*7, 7*7*7)
+	fmt.Printf("%d\t%d\t%d\n", 8, 8*8, 8*8*8)
+	fmt.Printf("%d\t%d\t%d\n", 9, 9*9, 9*9*9)
+	fmt.Printf("%d\t%d\t%d\n", 10, 10*10, 10*10*10)
+}
