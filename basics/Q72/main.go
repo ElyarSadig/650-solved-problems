faaed8c84ced8d32bd66faff8985513e90496c4d
elyar <elyarnejati@gmail.com>
2024-07-27 10:57:40 +0330

    add Q72

---
+++ basics/Q72/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Minimum Integer:", math.MinInt64)
+	fmt.Println("Maximum Integer:", math.MaxInt64)
+	fmt.Println("Maximum Float64:", math.MaxFloat64)
+}
