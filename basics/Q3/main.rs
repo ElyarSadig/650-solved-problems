8e345a1a31d6200d5a582e9792ba2b038b31c77f
elyar <elyarnejati@gmail.com>
2024-09-25 23:19:03 +0330

    add Q3 Rust

---
+++ basics/Q3/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    const PI: f64 = 22.0 / 7.0; 
+    let mut input = String::new();
+
+    println!("Enter Radius:");
+    io::stdin().read_line(&mut input).expect("could not read line");
+    let radius: f64 = input.trim().parse().expect("Not a valid number");
+
+    let volume = (4.0 / 3.0) * PI * radius * radius * radius;
+    let surface_area = 4.0 * PI * radius * radius;
+    
+    println!("Surface Area is: {surface_area}");
+    println!("Volume is: {volume}");
+}
