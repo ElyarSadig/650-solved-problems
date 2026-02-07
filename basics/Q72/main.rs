d704049ef49b1789551564ff38526d2af73153b5
ElyarSadig <elyarnejati@gmail.com>
2026-02-07 11:02:55 +0330

    Add Q72 rs

---
+++ basics/Q72/main.rs
@@ -0,0 +1,8 @@
+use std::i64;
+use std::f64;
+
+fn main() {
+    println!("Minimum Integer: {}", i64::MIN);
+    println!("Maximum Integer: {}", i64::MAX);
+    println!("Maximum Float64: {}", f64::MAX);
+}
